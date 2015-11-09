71bbb5cb4ef1160308d7d29e7993315582845c34
Erick Alfaro Hernández <erick.hdez06@outlook.com>
2015-11-09 12:12:02 -0600

    Llaves foraneas y tabla recibo

--- Query/CreacionTablas_DB.sql
+++ Query/CreacionTablas_DB.sql
@@ -4,41 +4,74 @@ GO
 USE FondoAhorrosDB;
 GO
 
-CREATE TABLE Deudores(
+CREATE TABLE Deudores (
+ID INT IDENTITY(1, 1) NOT NULL,
+Cedula INT,
+Nombre VARCHAR(100),
+PRIMARY KEY (ID)
+);
+GO
+
+CREATE TABLE TipoPrestamo (
 ID INT IDENTITY(1, 1) NOT NULL,
 Nombre VARCHAR(100),
-PRIMARY KEY(ID)
+Tasa FLOAT,
+Plazo INT,
+PRIMARY KEY (ID)
 );
 GO
 
-CREATE TABLE MovimientoInteresDiario(
+CREATE TABLE TipoMovimientoInteresDiario (
 ID INT IDENTITY(1, 1) NOT NULL,
+Nombre VARCHAR(100),
+PRIMARY KEY (ID)
+);
+GO
+
+CREATE TABLE MovimientoInteresDiario (
+ID INT IDENTITY(1, 1) NOT NULL,
+FK_TipoMovimientoInteresDiario INT,
 Fecha DATE,
 Monto INT,
-PRIMARY KEY(ID)
+PRIMARY KEY (ID),
+FOREIGN KEY (FK_TipoMovimientoInteresDiario) REFERENCES TipoMovimientoInteresDiario (ID)
 );
 GO
 
-CREATE TABLE MovimientosSaldoAplicado(
+CREATE TABLE MovimientoSaldoNoAplicado (
 ID INT IDENTITY(1, 1) NOT NULL,
 Fecha DATE,
 Amortizacion INT,
 Intereses INT,
-PRIMARY KEY(ID),
+PRIMARY KEY (ID),
 );
 GO
 
-CREATE TABLE MovimientoSaldoNoAplicado(
+CREATE TABLE Recibo (
+ID INT IDENTITY(1,  1) NOT NULL,
+FK_MovimientoSaldoNoAplicado INT,
+Texto VARCHAR(500),
+Estado BIT,
+PRIMARY KEY (ID),
+FOREIGN KEY (FK_MovimientoSaldoNoAplicado) REFERENCES MovimientoSaldoNoAplicado (ID)
+);
+
+CREATE TABLE MovimientoSaldoAplicado (
 ID INT IDENTITY(1, 1) NOT NULL,
 Fecha DATE,
 Amortizacion INT,
 Intereses INT,
-PRIMARY KEY(ID),
+PRIMARY KEY (ID),
 );
 GO
 
-CREATE TABLE Prestamos(
+CREATE TABLE Prestamos (
 ID INT IDENTITY(1, 1) NOT NULL,
+FK_Deudor INT,
+FK_TipoPrestamo INT,
+FK_MovimientoInteresDiario INT,
+FK_MovimientoSaldoNoAplicado INT,
+FK_MovimientoSaldoAplicado INT,
 MontoOriginal INT,
 Cuota INT,
 PlazoRestante INT,
@@ -47,21 +80,10 @@ SaldoAplicado INT,
 InteresAcumuladoMensual INT,
 DiaCorte INT,
 PRIMARY KEY(ID),
+FOREIGN KEY (FK_Deudor) REFERENCES Deudores (ID),
+FOREIGN KEY (FK_TipoPrestamo) REFERENCES TipoPrestamo (ID),
+FOREIGN KEY (FK_MovimientoInteresDiario) REFERENCES MovimientoInteresDiario (ID),
+FOREIGN KEY (FK_MovimientoSaldoNoAplicado) REFERENCES MovimientoSaldoNoAplicado (ID),
+FOREIGN KEY (FK_MovimientoSaldoAplicado) REFERENCES MovimientoSaldoAplicado (ID)
 );
 GO
-
-CREATE TABLE TipoMovimientoInteresDiario(
-ID INT IDENTITY(1, 1) NOT NULL,
-Nombre VARCHAR(100),
-PRIMARY KEY(ID)
-);
-GO
-
-CREATE TABLE TipoPrestamo(
-ID INT IDENTITY(1, 1) NOT NULL,
-Nombre VARCHAR(100),
-Tasa INT,
-Plazo INT,
-Periodo INT,
-PRIMARY KEY(ID)
-);
