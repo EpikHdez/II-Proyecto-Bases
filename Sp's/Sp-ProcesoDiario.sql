13ed799b2347cc23defbe0a5ca60de2ed0bf2795
luisalonzocascante <luisalonzocascante@gmail.com>
2015-11-12 19:09:44 -0600

    DiaPago

--- Sp's/Sp-ProcesoDiario.sql
+++ Sp's/Sp-ProcesoDiario.sql
@@ -9,7 +9,7 @@ BEGIN
 	DECLARE @FechaProceso DATE = CONVERT(DATE, GETDATE());
 
 	INSERT MovimientoInteresDiario(FK_TipoMovimientoInteresDiario, FK_Prestamo, Fecha, Monto, Activo)
-	SELECT 1, PR.ID, @FechaProceso, dbo.CalcularIntereses(PR.SaldoNoAplicado, TP.tasa), 1
+	SELECT 1, PR.ID, @FechaProceso, dbo.FAFN_CalcularInteres(PR.SaldoNoAplicado, TP.tasa), 1
 	FROM Prestamos PR INNER JOIN dbo.TipoPrestamo TP
 	ON PR.FK_TipoPrestamo = TP.ID
 	WHERE PR.SaldoNoAplicado > 0 AND (@FechaProceso BETWEEN PR.FechaInicio AND DATEADD(YEAR, TP.Plazo, PR.FechaInicio));
